35017b4ac0827240ca4ac1c0d35749052cece9dc
aloneking <108918854+aloneking5388@users.noreply.github.com>
2023-06-12 14:38:52 +0530

    add new

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://easyforpocket.com/api/v2";
+export const server = "https://easyforpocket.onrender.com/api/v2";
 
-export const backend_url = "https://easyforpocket.com/";
+export const backend_url = "https://easyforpocket.onrender.com/";
